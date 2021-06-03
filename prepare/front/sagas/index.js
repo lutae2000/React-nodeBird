dcbe6f2ba49dfac78b7d8ab5b4da5a7ca0e972b8
Lee Eui Tae <uitae@uitaes-MacBookAir.local>
2021-06-04 00:53:19 +0900

    saga 일부분 완성

--- prepare/front/sagas/index.js
+++ prepare/front/sagas/index.js
@@ -1,4 +1,4 @@
-import { all, call, fork, take, put } from 'redux-saga/effects';
+import { all, call, fork, put, takeLatest, throttle, delay } from 'redux-saga/effects';
 
 function loginAPI() {
     return axios.post('api/login');
@@ -6,8 +6,9 @@ function loginAPI() {
 
 function login() {
     try {
+        yield delay(1000);
         //call: 동기함수 호출, fork: 비동기 함수 호출
-        const result = yield call(loginAPI);
+        //const result = yield call(loginAPI, action.data);
         yield put({
             type: 'LOG_IN_SUCCESS',
             data: result.data,
@@ -26,8 +27,10 @@ function logoutAPI() {
 }
 function logout() {
     try {
+        yield delay(1000);
+
         //call: 동기함수 호출, fork: 비동기 함수 호출
-        const result = yield call(loginAPI);
+        //const result = yield call(loginAPI);
         yield put({
             type: 'LOG_OUT_SUCCESS',
             data: result.data,
@@ -41,13 +44,14 @@ function logout() {
     }
 }
 
-function addPostAPI() {
-    return axios.post('api/login');
+function addPostAPI(data) {
+    return axios.post('api/login', data);
 }
-function addPost() {
+function addPost(action) {
     try {
+        yield delay(1000);
         //call: 동기함수 호출, fork: 비동기 함수 호출
-        const result = yield call(loginAPI);
+        //const result = yield call(addPostAPI, action.data);
         yield put({
             type: 'ADD_POST__SUCCESS',
             data: result.data,
@@ -60,17 +64,21 @@ function addPost() {
         });
     }
 }
-
+//takeEvery: 응답에 상관없이 트랜젝션 처리도중에도 계속 받음
+//takeLatest : 트랜젝션 처리중 요청은 그대로 되고, 응답은 취소 됨
 function watchLogin() {
-    yield take('LOG_IN_REQUEST', login);
+    yield takeLatest('LOG_IN_REQUEST', login);
 }
 
 function* watchLogout() {
-    yield take('LOG_OUT_REQUEST', logout);
+    yield takeLatest('LOG_OUT_REQUEST', logout);
 }
 
 function* watchAddPost() {
-    yield take('ADD_POST_REQUEST', addPost);
+    yield takeLatest('ADD_POST_REQUEST', addPost);
+
+    //아래와 같이 사용하면 여러번 이벤트 발생해도 2초내에 1번만 실행 가능하게 할 수 있음
+    //yield throttle('ADD_POST_REQUEST', addPost, 2000);
 }
 
 export default function* rootSaga() {
